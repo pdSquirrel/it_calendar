5b241b2fa44ce848dcf0f0ec1936defb4cb63d0d
Roger Nordqvist <progdev.squirrel@gmail.com>
2021-11-15 11:45:09 +0100

    Added bootstrap

--- it_calendar/events/views.py
+++ it_calendar/events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
+from datetime import datetime
 
 # To do this in swedish, handle with locale
 # >>> import locale
@@ -12,8 +13,8 @@ from calendar import HTMLCalendar
 # >>> calendar.day_name[1]
 # 'Dienstag'
 
-def home(request, year, month):
-    month = month.capitalize() # Make sure first letter is capitalized in the string
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    month = month.capitalize()
     # Convert month from name to number
     month_number = list(calendar.month_name).index(month)
     month_number = int(month_number)
@@ -22,7 +23,7 @@ def home(request, year, month):
     cal = HTMLCalendar().formatmonth(year, month_number)
 
     
-    return render(request, 'home.html', {
+    return render(request, 'events/home.html', {
         "year": year,
         "month": month,
         "month_number": month_number,
