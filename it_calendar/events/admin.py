e9ea73b342f7d0b7ec6712a9f77b4c9f80bd93d0
Roger Nordqvist <progdev.squirrel@gmail.com>
2021-11-16 13:15:34 +0100

    added DB tables

--- it_calendar/events/admin.py
+++ it_calendar/events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Venue
+from .models import EventUser
+from .models import Event
 
-# Register your models here.
+admin.site.register(Venue)
+admin.site.register(EventUser)
+admin.site.register(Event)
